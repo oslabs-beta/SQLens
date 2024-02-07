a473227e2d3596e228f4fbd1301c8bd5bd283453
Jenny Ouk <jenny.ouk@gmail.com>
2024-02-06 18:44:18 -0800

    still bug hunting

--- src/DemoApp.tsx
+++ src/DemoApp.tsx
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
 import './stylesheets/flowStyle.css';
 import Flow from './components/Flow';
+// import LandingPage from './components/LandingPage';
+import {
+  BrowserRouter as DefaultRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import useStore from './store';
 import starterTables from './components/starterTables';
-import MigrationLog from './components/MigrationLog';
+// import { useQuery } from '@apollo/client';
+// import { GET_TABLE_NAMES } from './utilities/queries';
 
 const theme = createTheme({
   palette: {
@@ -15,17 +22,21 @@ const theme = createTheme({
   },
 });
 
-function App() {
+function App({ router: RouterComponent = DefaultRouter }) {
   const setTables = useStore((state) => state.setTables);
 
   useEffect(() => {
-      setTables(starterTables);
+    setTables(starterTables);
   }, [setTables]);
 
   return (
     <ThemeProvider theme={theme}>
-      <Flow />
-      <MigrationLog />
+      <RouterComponent>
+        <Routes>
+          {/* <Route path="/" element={<LandingPage />} /> */}
+          <Route path='/' element={<Flow />} />
+        </Routes>
+      </RouterComponent>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './DemoApp.tsx';
-// import './stylesheets/index.css';
+import './stylesheets/flowStyle.css';
 // import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 // const client = new ApolloClient({
